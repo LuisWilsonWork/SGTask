22f6e67a5024da228128488fb88b287759c2d856
Luis Wilson <Luis.Wilson.Work@outlook.com>
2021-08-17 14:09:10 +0100

    Time Exceeded- Will continue as a learning excercise. Response has been received successfully Front end needs to output each of the responses

--- SGTask/Controllers/MultiAgePredictionController.cs
+++ SGTask/Controllers/MultiAgePredictionController.cs
@@ -26,6 +26,8 @@ namespace SGTask.Controllers
         [HttpGet("{names}")]
         public MultiAgePrediction Get(string names)
         {
+            
+            
             var client = new RestClient("https://api.agify.io/");
 
             var nameList = names.Split(',');
@@ -39,10 +41,27 @@ namespace SGTask.Controllers
             var request = new RestRequest(requestString, Method.GET);
 
             RestResponse response = (RestResponse)client.Execute(request);
+            //Was unable to parse the API Response to the correct data type so i am doing it in a messy way to reach a functional conclusion
+            var namesSplit = response.Content.Split(new string[] { "{\"name\":\"" }, StringSplitOptions.None);
 
-            JsonDeserializer deserial = new JsonDeserializer();
-            var prediction = JsonConvert.DeserializeObject<MultiAgePrediction>(response.ToString());
-            
+            List<AgePrediction> predictionList = new List<AgePrediction>();
+
+            for (int i = 1; i < namesSplit.Length; i++)
+            {
+                var nameAgeSplit =  namesSplit[i].Split(new string[] { "\",\"age\":" }, StringSplitOptions.None);
+                //Name is saved. the age and count are remaining.
+                string name = nameAgeSplit[0];
+                var agesSplit = nameAgeSplit[1].Split(new string[] { "," }, StringSplitOptions.None);
+                var age = agesSplit[0];
+
+                AgePrediction a = new AgePrediction { Name = name, Age = int.Parse(age) };
+                predictionList.Add(a);
+            }
+
+            MultiAgePrediction prediction = new MultiAgePrediction();
+
+
+
             return prediction;
         }
     }
