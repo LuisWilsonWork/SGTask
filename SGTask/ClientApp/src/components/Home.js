0147c78ecc62709a7ca343966deebcf167a29083
Luis Wilson <Luis.Wilson.Work@outlook.com>
2021-08-17 13:29:52 +0100

    Correct call to API is being made by MultiAge Controller Unable to deserialise the object into the wanted datatype to sent to the frontend

--- SGTask/ClientApp/src/components/Home.js
+++ SGTask/ClientApp/src/components/Home.js
@@ -7,20 +7,24 @@ export class Home extends Component {
     return (
       <div>
             <h1>Age predictions based on a name!</h1>
-            <br/>
+            <br />
+
             <label>Input a single name below</label>
             <br/>
             <input id="nameInput"></input>
             <button onClick={() => { this.GetAgePrediction() }} >Get Age Prediction</button>
             <br />
             <br />
+
             <label>Input Multiple names below seperated by a ','</label>
             <br />
-            <input id="nameInput"></input>
-            <button onClick={() => { this.GetAgePrediction() }} >Get Age Prediction</button>
+            <input id="multiInput"></input>
+            <button onClick={() => { this.GetMultiAgePrediction() }} >Get Age Predictions</button>
       </div>
     );
     }
+
+    
 
     async GetAgePrediction() {
         let inputProperty = document.getElementById("nameInput")
@@ -28,4 +32,13 @@ export class Home extends Component {
         const data = await response.json();
         alert("Predicted age for " + data.name + " is : " + data.age);
     }
+
+    async GetMultiAgePrediction() {
+        let inputProperty = document.getElementById("multiInput")
+        const response = await fetch('multiageprediction/' + inputProperty.value);
+        const data = await response.json();
+        alert("Predicted age for " + data.name + " is : " + data.age);
+    }
+
+    
 }
---
+++ SGTask/Controllers/MultiAgePredictionController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MultiAgePredictionController : ControllerBase
+    {
+
+
+        private readonly ILogger<MultiAgePredictionController> _logger;
+
+        public MultiAgePredictionController(ILogger<MultiAgePredictionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{names}")]
+        public MultiAgePrediction Get(string names)
+        {
+            var client = new RestClient("https://api.agify.io/");
+
+            var nameList = names.Split(',');
+            string requestString = "?";
+
+            foreach (string name in nameList)
+            {
+                requestString += "name[]=" + name + "&"; // API Works with the extra ampersand. generally would remove the final one however functionality remains
+            }
+
+            var request = new RestRequest(requestString, Method.GET);
+
+            RestResponse response = (RestResponse)client.Execute(request);
+
+            JsonDeserializer deserial = new JsonDeserializer();
+            var prediction = JsonConvert.DeserializeObject<MultiAgePrediction>(response.ToString());
+            
+            return prediction;
+        }
+    }
+}
