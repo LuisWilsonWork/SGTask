024e2d72b0e058489327ab1796e53fa85497f3db
Luis Wilson <Luis.Wilson.Work@outlook.com>
2021-08-17 12:05:53 +0100

    Created Class and Controller for name age prediction

---
+++ SGTask/AgePrediction.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SGTask
+{
+    public class AgePrediction
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        
+    }
+}
---
+++ SGTask/Controllers/AgePredictionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AgePredictionController : ControllerBase
+    {
+        
+
+        private readonly ILogger<AgePredictionController> _logger;
+
+        public AgePredictionController(ILogger<AgePredictionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public AgePrediction Get()
+        {
+            return new AgePrediction
+            {
+                Name = "Luis",
+                Age = 26
+                
+            };
+
+
+
+
+        }
+    }
+}
